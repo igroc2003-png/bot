36113417a565d69b4409dd9b7505b1d1de2de3ea
igroc2003-png <igroc2003@gmail.com>
2026-01-06 21:49:29 +0700

    app.py

--- app.py
+++ app.py
@@ -3,31 +3,39 @@ import requests
 import os
 
 app = Flask(__name__)
-TOKEN = os.getenv("f9LHodD0cOKVZp7gFirF4PSYvSvITJ-N_iM68WvIJYslb6uURgZkHTfMAxC4a_8CxH0JdE1CnFu9w5qjQEcv")
 
+TOKEN = os.getenv("MAX_BOT_TOKEN")
 MAX_API = f"https://api.max.ru/bot{TOKEN}"
 
 def send_message(chat_id, text):
-    requests.post(f"{MAX_API}/sendMessage", json={
-        "chat_id": chat_id,
-        "text": text
-    })
+    requests.post(
+        f"{MAX_API}/sendMessage",
+        json={
+            "chat_id": chat_id,
+            "text": text
+        },
+        timeout=5
+    )
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
     data = request.json
-    if not data:
-        return {"ok": False}
+    print(data)
 
-    message = data.get("message")
-    if message:
-        chat_id = message["chat"]["id"]
-        text = message.get("text", "")
-        
-        if text == "/start":
-            send_message(chat_id, "Привет! Бот работает!")
-        else:
-            send_message(chat_id, f"Ты написал: {text}")
+    if not data:
+        return {"ok": True}
+
+    if "object" not in data or "message" not in data["object"]:
+        return {"ok": True}
+
+    message = data["object"]["message"]
+    chat_id = message["chat"]["id"]
+    text = message.get("text", "")
+
+    if text == "/start":
+        send_message(chat_id, "✅ Привет! Бот работает!")
+    else:
+        send_message(chat_id, f"Ты написал: {text}")
 
     return {"ok": True}
 
