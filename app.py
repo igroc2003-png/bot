11f3a6b8961881186c40b3eafc6378749a33c796
igroc2003-png <igroc2003@gmail.com>
2026-01-06 19:05:29 +0700

    app.py

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
 from flask import Flask, request
+import requests
+import os
 
 app = Flask(__name__)
+TOKEN = os.getenv("MAX_BOT_TOKEN")
+
+MAX_API = f"https://api.max.ru/bot{TOKEN}"
+
+def send_message(chat_id, text):
+    requests.post(f"{MAX_API}/sendMessage", json={
+        "chat_id": chat_id,
+        "text": text
+    })
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
     data = request.json
-    print(data)  # просто лог для проверки
-    return "ok"
+    if not data:
+        return {"ok": False}
+
+    message = data.get("message")
+    if message:
+        chat_id = message["chat"]["id"]
+        text = message.get("text", "")
+        
+        if text == "/start":
+            send_message(chat_id, "Привет! Бот работает!")
+        else:
+            send_message(chat_id, f"Ты написал: {text}")
+
+    return {"ok": True}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
