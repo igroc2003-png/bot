b5361104aae023c11c851a8eb66557e744072872
igroc2003-png <igroc2003@gmail.com>
2026-01-07 11:09:47 +0700

    app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask, request
-import requests
 import os
 
 app = Flask(__name__)
 
-TOKEN = os.getenv("MAX_BOT_TOKEN")
-MAX_API = f"https://api.max.ru/bot{TOKEN}"
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
 @app.route("/", methods=["GET"])
 def index():
@@ -13,28 +11,9 @@ def index():
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    data = request.json
-    print(data)
-
-    if not data:
-        return {"ok": True}
-
-    if "object" not in data or "message" not in data["object"]:
-        return {"ok": True}
-
-    message = data["object"]["message"]
-    chat_id = message["chat"]["id"]
-    text = message.get("text", "")
-
-    requests.post(
-        f"{MAX_API}/sendMessage",
-        json={"chat_id": chat_id, "text": f"Ты написал: {text}"},
-        timeout=5
-    )
-
+    print(request.json)  # выводим в консоль все данные
     return {"ok": True}
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 3000))
     app.run(host="0.0.0.0", port=port)
-
